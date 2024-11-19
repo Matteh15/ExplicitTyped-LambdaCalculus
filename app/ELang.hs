dc9d516946c8fc18a87aa5b03e1947de9c3c0490
Matteo <matteo.lalicata00@gmail.com>
2024-11-19 19:02:13 +0100

    Small refactor

--- app/ELang.hs
+++ app/ELang.hs
@@ -1,43 +1,48 @@
 module ELang where
 
-data Exp = Tru
-        | Fls 
-        | IfThenElse Exp Exp Exp 
-        | Zero 
-        | Succ Exp
-        | Pred Exp 
-        | IsZero Exp 
-    deriving Show
+data Exp =
+  T
+  | F 
+  | Z 
+  | IfThenElse Exp Exp Exp 
+  | Succ Exp
+  | Pred Exp 
+  | IsZero Exp 
+  deriving Show
 
 isNum :: Exp -> Bool
-isNum Zero     = True
+isNum Z     = True
 isNum (Succ n) = isNum n
 isNum _        = False
 
 isVal :: Exp -> Bool
-isVal Tru = True
-isVal Fls = True
-isVal t   = isNum t
+isVal T = True
+isVal F = True
+isVal t = isNum t
 
 reduce :: Exp -> Maybe Exp
-reduce Tru = Nothing
-reduce Fls = Nothing
-reduce (IfThenElse Tru t1 _) = return t1
-reduce (IfThenElse Fls _ t2) = return t2
-reduce (IfThenElse t t1 t2)  = do 
+reduce T = Nothing
+reduce F = Nothing
+reduce Z = Nothing
+
+reduce (IfThenElse T t1 _)  = return t1
+reduce (IfThenElse F _ t2)  = return t2
+reduce (IfThenElse t t1 t2) = do 
   t' <- reduce t
   return (IfThenElse t' t1 t2)
-reduce Zero = Nothing
+
 reduce (Succ t) = do
  t' <- reduce t
  return (Succ t')
-reduce (Pred Zero)               = return Zero
+
+reduce (Pred Z) = return Z
 reduce (Pred (Succ n)) | isNum n = return n
 reduce (Pred t) = do
  t' <- reduce t
  return (Pred t')
-reduce (IsZero Zero)               = return Tru
-reduce (IsZero (Succ t)) | isNum t = return Fls
+
+reduce (IsZero Z) = return T
+reduce (IsZero (Succ t)) | isNum t = return F
 reduce (IsZero t) = do
  t' <- reduce t
  return (IsZero t')
@@ -49,4 +54,4 @@ reduceStar t =
       Just t' -> reduceStar t'
       _       -> t
 
-t = reduceStar (IfThenElse (IsZero (Succ (Pred Zero))) (Pred (Succ Zero)) Zero)
+t = reduceStar (IfThenElse (IsZero (Succ (Pred Z))) (Pred (Succ Z)) Z)
