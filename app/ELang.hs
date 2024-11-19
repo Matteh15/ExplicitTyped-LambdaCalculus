d8d561117b58736843bcb15d57621dcbfc468795
Matteo <matteo.lalicata00@gmail.com>
2024-11-19 21:29:04 +0100

    Add lambda calculus

--- app/ELang.hs
+++ app/ELang.hs
@@ -8,6 +8,9 @@ data Exp =
   | Succ Exp
   | Pred Exp 
   | IsZero Exp 
+  | Var String
+  | LambdaAbs String Exp
+  | App Exp Exp
   deriving Show
 
 isNum :: Exp -> Bool
@@ -18,7 +21,40 @@ isNum _        = False
 isVal :: Exp -> Bool
 isVal T = True
 isVal F = True
+isVal (LambdaAbs _ _) = True
 isVal t = isNum t
+
+freeVariables :: Exp -> [String]
+freeVariables (Var x) = [x]
+freeVariables (LambdaAbs x t) = filter (/= x) (freeVariables t)
+freeVariables (App t1 t2) = freeVariables t1 ++ freeVariables t2
+freeVariables (IfThenElse t1 t2 t3) = freeVariables t1 ++ freeVariables t2 ++ freeVariables t3
+freeVariables (Succ t) = freeVariables t
+freeVariables (Pred t) = freeVariables t
+freeVariables (IsZero t) = freeVariables t
+freeVariables _ = []
+
+freshVariable :: String -> [String] -> String
+freshVariable x xs =
+  let candidates = [x ++ show i | i <- [1..]]
+  in head (filter (`notElem` xs) candidates)
+
+substitute :: String -> Exp -> Exp -> Exp
+substitute x s (Var y) 
+  | x == y = s
+  | otherwise = Var y
+substitute x s (LambdaAbs y t)
+  | x == y = LambdaAbs y t
+  | y `elem` freeVariables s = 
+    let y' = freshVariable y (freeVariables s ++ freeVariables t)
+    in LambdaAbs y' (substitute x s (substitute y (Var y') t))
+  | otherwise = LambdaAbs y (substitute x s t)
+substitute x s (App t1 t2) = App (substitute x s t1) (substitute x s t2)
+substitute x s (IfThenElse t1 t2 t3) = IfThenElse (substitute x s t1) (substitute x s t2) (substitute x s t3)
+substitute x s (Succ t) = Succ (substitute x s t)
+substitute x s (Pred t) = Pred (substitute x s t)
+substitute x s (IsZero t) = IsZero (substitute x s t)
+substitute _ _ t = t
 
 reduce :: Exp -> Maybe Exp
 reduce T = Nothing
@@ -46,6 +82,21 @@ reduce (IsZero (Succ t)) | isNum t = return F
 reduce (IsZero t) = do
  t' <- reduce t
  return (IsZero t')
+
+reduce (App (LambdaAbs x t1) v2) | isVal v2 = return (substitute x v2 t1)
+reduce (App t1 t2) | isVal t1 = do
+ t2' <- reduce t2
+ return (App t1 t2')
+reduce (App t1 t2) = do
+  t1' <- reduce t1
+  return (App t1' t2)
+
+reduce (LambdaAbs x t) = do
+  t' <- reduce t
+  return (LambdaAbs x t')
+
+reduce (Var x) = Nothing
+
 
 reduceStar :: Exp -> Exp
 reduceStar t = 
--- app/main.hs
+++ app/main.hs
@@ -4,5 +4,8 @@ import ELang
 
 main :: IO ()
 main = do
-    let t = reduceStar (IfThenElse Tru (Succ Zero) Zero)
+    let f0 = LambdaAbs "x" (LambdaAbs "y" (IfThenElse (IsZero (Var "x")) (Var "y") F))
+    let f1 = App f0 $ Succ Z
+    let f2 = LambdaAbs "x" (Var "x")
+    let t = reduceStar $ App f2 T
     print t
